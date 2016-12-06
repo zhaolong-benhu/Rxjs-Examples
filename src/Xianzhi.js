6005de79ecffd068cac35d8151958a5422a0e478
zhaolong <zhaolong@dfwsgroup.com>
2016-12-06 14:15:56 +0800

    add Xianzhi.js

---
+++ src/Xianzhi.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+class Xianzhi extends React.Component{
+
+
+    constructor(props){
+        super(props)
+        this.state={
+            items:[]
+        }
+    }
+
+
+    render(){
+        return <div>
+            Welcome to Xianzhi-platform 
+        </div>
+ }
+
+}
+
+export  default Xianzhi
